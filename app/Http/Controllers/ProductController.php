acdad8fe623fb9f8b5ce82da2c89563e1ca9cfe7
Abhijit Chanda <abhijitcse32@gmail.com>
2021-03-04 21:43:25 +0600

    Create ProductController.php

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Product;
+
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+
+    public function store(Request $request)
+    {
+        $product = new Product([
+          'title' => $request->get('title'),
+          'body' => $request->get('body')
+        ]);
+        $product->save();
+
+
+        return response()->json('Product Added Successfully.');
+    }
+	
+	public function edit($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+	
+	public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->title = $request->get('title');
+        $product->body = $request->get('body');
+        $product->save();
+
+
+        return response()->json('Product Updated Successfully.');
+    }
+
+	public function destroy($id)
+    {
+      $product = Product::find($id);
+      $product->delete();
+
+
+      return response()->json('Product Deleted Successfully.');
+    }
+}
